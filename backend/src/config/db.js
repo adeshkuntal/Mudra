c30d700ce5c2b4ea2283156bccb313dfcb82ea8e
gouravsharma-00 <gourav.sharma.glamat2932@gmail.com>
2025-08-15 13:20:00 +0000

    init branch gourav

---
+++ backend/src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+export default connectDB = async (mongoUri) => {
+  try {
+    const conn = await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
